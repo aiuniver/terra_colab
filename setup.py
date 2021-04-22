63d7301fd2f1c4c0ba90eb0051f14be75faa5646
BL146U <bl146u@gmail.com>
2021-04-22 18:40:37 +0300

    Changes

--- setup.py
+++ setup.py
@@ -10,4 +10,7 @@ setup(
     version=__version__,
     packages=find_packages(),
     long_description=open(join(dirname(__file__), "README.md")).read(),
+    install_requires=[
+        "dill",
+    ],
 )
--- terra_colab/launcher_base.py
+++ terra_colab/launcher_base.py
@@ -1,6 +1,13 @@
+from os import chdir
 from typing import Optional, Any
 
 
 class LauncherBase:
     def getup(self, dataset: Optional[Any] = None):
-        print(f'Launcher getup with dataset "{dataset} -> {type(dataset)}"')
+        if dataset:
+            print(f'Launch with dataset "{dataset} -> {type(dataset)}"')
+        else:
+            print("Launch without dataset")
+
+        chdir
+        %cd / content
