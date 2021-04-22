1f37b16a128344f6be81f2ca4a561a8dc6bf556a
BL146U <bl146u@gmail.com>
2021-04-22 19:55:10 +0300

    Changes

--- setup.py
+++ setup.py
@@ -12,6 +12,6 @@ setup(
     long_description=open(join(dirname(__file__), "README.md")).read(),
     install_requires=[
         "dill>=0.3.3",
-        "requests>=2.25.1",
+        "requests~=2.23.0",
     ],
 )
