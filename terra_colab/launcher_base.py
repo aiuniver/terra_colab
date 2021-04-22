82899604430d01c33597d940daa42f728885abcb
BL146U <bl146u@gmail.com>
2021-04-22 16:52:26 +0300

    Command

--- terra_colab/launcher_base.py
+++ terra_colab/launcher_base.py
@@ -1,6 +1,7 @@
 class LauncherBase:
     def getup(self, dataset: str = ""):
         print(f'Launcher getup with dataset "{dataset}[type={type(dataset)}]"')
+        print(globals().keys())
         dataset = globals().get(dataset, None)
         if dataset:
             print("Dataset is undefined")
