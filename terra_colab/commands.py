934da1506a662ebb7c137ae339204c8e735a5a28
BL146U <bl146u@gmail.com>
2021-04-22 17:33:17 +0300

    Changes

--- terra_colab/commands.py
+++ terra_colab/commands.py
@@ -1,13 +1,5 @@
-import argparse
-
 from . import launcher
 
 
 def getup():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--dataset", type=str, help="Variable name of instance class DTS"
-    )
-    args = parser.parse_args()
-    dataset = args.dataset or ""
-    launcher.getup(dataset)
+    launcher.getup()
--- terra_colab/launcher_base.py
+++ terra_colab/launcher_base.py
@@ -1,9 +1,16 @@
+from typing import Optional, Any
+
+
 class LauncherBase:
-    def getup(self, dataset: str = ""):
-        print(f'Launcher getup with dataset "{dataset}[type={type(dataset)}]"')
-        print(globals().keys())
-        dataset = globals().get(dataset, None)
-        if dataset:
-            print("Dataset is undefined")
-        else:
-            print("Dataset loaded:", dataset, type(dataset))
+    dataset: Optional[Any] = None
+
+    def getup(self):
+        print(dir(self))
+        print(self.dataset)
+        print(type(self.dataset))
+        # print(f'Launcher getup with dataset "{dataset}[type={type(dataset)}]"')
+        # dataset = globals().get(dataset, None)
+        # if dataset:
+        #     print("Dataset is undefined")
+        # else:
+        #     print("Dataset loaded:", dataset, type(dataset))
