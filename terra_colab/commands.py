5b0d464f099edeef27c9ea58c7305cfc16948215
BL146U <bl146u@gmail.com>
2021-04-23 12:49:10 +0300

    Changes

--- terra_colab/commands.py
+++ terra_colab/commands.py
@@ -3,15 +3,35 @@ import os
 
 from .launcher import launcher
 
+import sys
+import os, subprocess
+
+# p=subprocess.Popen('env',stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell = True)
+# oldEnv=p.communicate()[0]
+# p=subprocess.Popen('source /tmp/test/setenv.sh ; env',stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell = True)
+# newEnv=p.communicate()[0]
+# for newStr in newEnv.split('\n'):
+#     flag = True
+#     for oldStr in oldEnv.split('\n'):
+#         if newStr == oldStr:
+#             #not exported by setenv.sh
+#             flag = False
+#             break
+#     if flag:
+#         #exported by setenv.sh
+#         print newStr
+
 
 def auth():
-    os.environ["EMAIL"] = input("Введите E-mail: ")
-    os.environ["TOKEN"] = input("Введите Token: ")
+    # email = input("Введите E-mail: ")
+    # token = input("Введите Token: ")
     # drive.mount("/content/drive")
     # launcher.auth()
     # launcher = Launcher()
     # print(launcher)
     # print(globals().keys())
+    print(f"export EMAIL={email}")
+    print(f"export TOKEN={token}")
 
 
 def gdmount():
---
+++ terra_colab_auth.sh
@@ -0,0 +1,2 @@
+Введите E-mail: Введите Token: export EMAIL=asdas
+export TOKEN=asdasd
