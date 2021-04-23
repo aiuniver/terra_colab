cd13952cd288264b748939fe9fbf504529a7ad9d
BL146U <bl146u@gmail.com>
2021-04-23 11:31:12 +0300

    Changes

--- terra_colab/commands.py
+++ terra_colab/commands.py
@@ -1,8 +1,10 @@
+from google.colab import drive
 from .launcher import launcher
 
 
 def auth2shell():
-    launcher.auth()
+    drive.mount("/content/drive")
+    # launcher.auth()
     # launcher = Launcher()
     # print(launcher)
-    print(globals().keys())
+    # print(globals().keys())
