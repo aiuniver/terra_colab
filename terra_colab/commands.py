046e0d708d4970799b8916ec2f88614c3e1b9bc7
BL146U <bl146u@gmail.com>
2021-04-23 12:11:53 +0300

    Changes

--- terra_colab/commands.py
+++ terra_colab/commands.py
@@ -1,16 +1,12 @@
 # from google.colab import drive
 import os
-import subprocess
 
 from .launcher import launcher
 
 
 def auth():
-    email = input("Введите E-mail: ")
-    token = input("Введите Token: ")
-    ddd = subprocess.Popen(f"export EMAIL={email}; export TOKEN={token}")
-    print(ddd)
-    ddd.wait()
+    os.environ["EMAIL"] = input("Введите E-mail: ")
+    os.environ["TOKEN"] = input("Введите Token: ")
     # drive.mount("/content/drive")
     # launcher.auth()
     # launcher = Launcher()
