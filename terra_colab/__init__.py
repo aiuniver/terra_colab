17a5874c721817a1fd41c816e405319a6e7d21e5
BL146U <bl146u@gmail.com>
2021-04-23 17:18:12 +0300

    Changes

--- terra_colab/__init__.py
+++ terra_colab/__init__.py
@@ -1,6 +1,1 @@
 __version__ = "0.1"
-
-COLAB_CONTENT_PATH = "/content"
-COLAB_TERRA_GUI_PATH = f"{COLAB_CONTENT_PATH}/terra_gui"
-COLAB_AUTH_URL = "http://terra.neural-university.ru/api/v1/login/"
-COLAB_TERRA_GUI_GIT = "https://github.com/aiuniver/terra_gui.git"
