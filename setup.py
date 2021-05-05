2e466ddbfebda93083c6f98dce36e0d2ccea8bef
BL146U <bl146u@gmail.com>
2021-05-05 18:13:59 +0300

    Changes

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 from setuptools import setup, find_packages
-from os.path import join, dirname
 from terra_colab import __version__
 
 
@@ -9,7 +8,7 @@ setup(
     name="terra_colab",
     version=__version__,
     packages=find_packages(),
-    long_description=open(join(dirname(__file__), "README.md")).read(),
+    long_description=open("README.rst").read(),
     install_requires=[
         "dill>=0.3.3",
         "requests~=2.23.0",
