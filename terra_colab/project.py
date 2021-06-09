5e5a4472bb793c4e0c2f448292b90c97b29a8f7a
BL146U <bl146u@gmail.com>
2021-06-09 14:31:01 +0300

    Changes

--- terra_colab/project.py
+++ terra_colab/project.py
@@ -1,3 +1,8 @@
 class TerraProject:
-    def __init__():
-        pass
+    name: str
+
+    def __init__(self, name: str):
+        self.name = name
+
+    def __repr__(self):
+        print(f"{self.__class__}: {self.name}")
