98681700441c3962d87df233413aa098066d796e
BL146U <bl146u@gmail.com>
2021-09-15 08:33:21 +0300

    -

--- terra_colab/commands.py
+++ terra_colab/commands.py
@@ -1,2 +1,15 @@
+import os
+
+from pathlib import Path
+
+
+def _mount_google_drive(path: Path) -> bool:
+    print(path)
+    return False
+
+
 def init():
-    print("Init")
+    print(Path().resolve())
+    print(os.path.abspath(os.getcwd()))
+    if not _mount_google_drive(Path("/content/drive")):
+        return
